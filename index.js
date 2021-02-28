4af8e402d4b69fdb2ffa4520d13c87eca092d36f
shonkam <samu.honkamaki@gmail.com>
2021-02-28 22:15:00 +0200

    fifth

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.get('/products/beanies', async (request, response) => {
 })
 
 
-const PORT = process.env.PORT || 3001
+const PORT = (process.env.PORT || 3001)
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
